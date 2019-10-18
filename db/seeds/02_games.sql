584b38222742d90bd02b32777a018b4e90faa454
Alan Dodge <alphaechododge@gmail.com>
2019-10-18 21:45:07 +0000

    Added seeds

---
+++ db/seeds/02_games.sql
@@ -0,0 +1,2 @@
+INSERT INTO games (name, number_of_players) VALUES ('Goofspiel', 2);
+INSERT INTO users (name, number_of_players) VALUES ('War', 2);
---
+++ db/seeds/03_records.sql
@@ -0,0 +1,4 @@
+INSERT INTO records (winner, loser, game_id)
+VALUES ('Kevin', 'Steve', 1),
+('Steve', 'Kevin', 2),
+('Kevin', 'Sarah', 1);
