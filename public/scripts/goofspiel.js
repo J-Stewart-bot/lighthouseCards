c98bc06efd93651f46fee13b251c4103071326dc
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-10-19 23:19:01 +0000

    Can select a card from hand, gets card id, removes card from hand on confirm

--- public/scripts/goofspiel.js
+++ public/scripts/goofspiel.js
@@ -5,20 +5,29 @@ $(() => {
 
   // ask username
   const username = $("#username").text();
-  console.log(username);
   socket.emit('username', username);
 
   socket.on('turn', function(username) {
-    console.log(username, 'says your turn');
-    $('#turn').text('Your turn');
     $('#confirm').css('visibility', 'visible');
     document.title = "!!";
   });
 
   $('#confirm').click(() => {
     $('#confirm').css('visibility', 'hidden');
+    $('.player > .container').find('.selected').remove();
     document.title = "Goofspiel";
     socket.emit('turn', username);
+  })
+
+  $('.player > .container > .card').click(function() {
+    let clicked = $(`#${this.id}`).hasClass("selected");
+    $(".card").removeClass("selected");
+    if (clicked) {
+      $(`#${this.id}`).removeClass("selected");
+      clicked = false;
+    } else {
+      $(`#${this.id}`).addClass("selected");
+    }
   })
 
 });
