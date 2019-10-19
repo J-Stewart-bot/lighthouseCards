c6259af4398dec26cbe4788a9ec1ab484f9e02d1
Alan Dodge <alphaechododge@gmail.com>
2019-10-19 20:23:51 +0000

    Added socket.io to connect users

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const express    = require("express");
 const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
 const app        = express();
+const http       = require('http').Server(app);
+const io         = require('socket.io')(http);
 const morgan     = require('morgan');
 
 const cookieSession = require('cookie-session');
@@ -75,6 +77,24 @@ app.post('/logout', (req, res) => {
   res.redirect('/');
 })
 
-app.listen(PORT, () => {
+io.sockets.on('connection', function(socket) {
+  socket.on('username', function(username) {
+      socket.username = username;
+      console.log(username);
+      io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+  });
+
+  socket.on('disconnect', function(username) {
+      io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+  })
+
+  socket.on('chat_message', function(message) {
+      io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+  });
+
+});
+
+
+http.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
