24c0c61dc7b2a3621115e7712d7d88fb9d5e25f1
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-10-23 21:27:39 +0000

    created logic functions for speed

---
+++ games/speedCheck.js
@@ -0,0 +1,22 @@
+const checkCards = function(playerCard, pileCard) {
+  if (Math.abs(playerCard.value - pileCard.value) === 1) {
+    return true;
+  } else if (playerCard.face === 'K' && pileCard.face === 'A') {
+    return true;
+  } else if (playerCard.face === 'A' && pileCard.face === 'K') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const checkPiles = function(hand, piles) {
+  for (const playerCard of hand) {
+    for (const pileCard of piles) {
+      if (checkCards(playerCard, pileCard)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
