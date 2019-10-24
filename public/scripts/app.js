b0d51385a3bc71fa33c5fb2dc7913f9c07fc5eff
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-10-24 17:43:16 +0000

    new style scheme and smaller records now function and styled properly in all games

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ $(() => {
     url: "/games"
   }).done((games) => {
     for (const game of games.games) {
-      $("<button>").addClass(`${game.name.toLowerCase()} btn btn-outline-info game`).text(game.name).appendTo($("body"));
+      $("<button>").addClass(`${game.name.toLowerCase()} btn btn-dark game`).text(game.name).appendTo($("body"));
     }
     $(`.war`).click(function() {
       window.open('/games/war');
