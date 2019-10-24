4b0495b4acf568fc1a4b62b704531c3e50f9f2c6
Alan Dodge <adodge@Alans-MacBook-Pro.local>
2019-10-23 19:23:11 -0700

    Plz socket

--- public/scripts/gameScript.js
+++ public/scripts/gameScript.js
@@ -18,7 +18,7 @@ const flip = function(hand, leftCard, rightCard) {
 }
 
 $(() => {
-  const socket = io.connect('https://wasting-time-lighthouse-labs.herokuapp.com:8080');
+  const socket = io();
   // const socket = io.connect('192.168.1.3:8080');
 
   let leftCard;
